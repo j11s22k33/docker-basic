68c1a57e42bb47216db5348233a3c2ada2a996e0
j11s22k33 <j11s22k33@gmail.com>
2022-07-25 02:38:25 +0900

    1

--- docker/docker-run.bat
+++ docker/docker-run.bat
@@ -1,13 +1,37 @@
-set CONTAINER=mycontainer
-set IMAGE=j11s22k33/myimage
+set CONTAINER=myubuntu
+set IMAGE=ubuntu
 set TAG=latest
 
-docker rm %CONTAINER% --force
+docker rm ^
+	%CONTAINER% ^
+	--force
+
+::cmd /K
+
 docker run ^
-  --detach ^
-  --publish 8080:8080 ^
-  --publish 3000:3000 ^
-  --publish 443:443 ^
-  --publish 80:80 ^
-  --name %CONTAINER% ^
-  %IMAGE%:%TAG%
+	--detach ^
+	--privileged ^
+	--interactive ^
+	--tty ^
+    --publish 3000:3000 ^
+	--name %CONTAINER% ^
+	%IMAGE%:%TAG%
+	
+::cmd /K
+
+docker exec ^
+	--privileged ^
+	--interactive ^
+	--tty ^
+	%CONTAINER% ^
+	/bin/sh -c "apt update&& apt install -y git&& git clone https://github.com/j11s22k33/json-server-basic.git&& apt install -y curl&& curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | sh"
+	
+::cmd /K
+
+:: 여기서 에러 많이 발생
+docker exec ^
+	--privileged ^
+	--interactive ^
+	--tty ^
+	%CONTAINER% ^
+	/bin/sh -c ". ~/.nvm/nvm.sh&&nvm install --lts&&cd json-server-basic&&npm install&&npm run start"
