50613f8fbf4b837d5fab6d5a24c4bf05bf45ef58
j11s22k33 <j11s22k33@gmail.com>
2022-07-25 09:56:43 +0900

    1

--- docker/docker-console.bat
+++ docker/docker-console.bat
@@ -1,15 +1,15 @@
 :: "https://docs.docker.com/engine/reference/commandline/attach"
 
 set CONTAINER=mycontainer
-set CMD=/bin/sh
-
-docker attach %CONTAINER%
-
-exit /B
 
 docker exec ^
 	--privileged ^
 	--interactive ^
 	--tty ^
     %CONTAINER% ^
-    %CMD%
+    sh
+
+exit /B
+
+:: "컨테이너에 직접 붙임. Ctrl+] 로 detach. exit 하면 컨테이너도 종료됨."
+docker attach --detach-keys="ctrl-]" %CONTAINER%
