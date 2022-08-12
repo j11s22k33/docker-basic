f8f2a8f2a0c0acf360315599f6503701fff3f142
j11s22k33 <j11s22k33@gmail.com>
2022-08-12 18:17:52 +0900

    1

--- docker/create-img-ex1/docker-run.bat
+++ docker/create-img-ex1/docker-run.bat
@@ -27,11 +27,11 @@ docker exec ^
     %CONTAINER% ^
     /bin/bash -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAx7Im+nTpceDvGUAMSFNhBMWgaTWuKjOJwtslAOJdDmP9i/WdFckDOaYmB9Qci59tfte0dp9lz3D5MJWTrOGVdB5GKgXTNwQ4KxTS4C4UNPP40EaYy0eqF0MuGPx8DvyB6ftvTsWNcv5ZYQ5pWU8mPY40Qonscj2DWOs4VWJvpxJBBMPMdIZKB/GJ1b17Kt0AJdvDoRAJ0pdiqq0v4q0VjP4FBPhKlL/Z6NxVF4C123wKDihFeFjdOufOC5yekzrDtmOuIidRRVFUgIyWLVGrq565zg0B1ZzZkoxP+n2DrGN5HhXkB405EGrnTbcK5PufZgU5D65WgJJU4GYgbxrdzw==' >> ~/.ssh/authorized_keys"
 
-:: cat samsung-cms-key.pub >> ~/.ssh/authorized_keys		
+:: cat TvAppCmsDevelopment.pub >> ~/.ssh/authorized_keys		
 docker exec ^
 	--privileged ^
 	--user ubuntu ^
 	--interactive ^
 	--tty ^
     %CONTAINER% ^
-    /bin/bash -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCg9/eH0FNByfRBQpNBx8HmdSr47xeHC7FN4bI+guet5e4CFnZ/ByKEJC/aG0vVj2kcUJCvKFfhNuxkWKjSOahKzH2a60nco3b9IT5zSwFwIjogWXVkSF0c6JMsEPVcAw0beRLV0nedDXz2Dns9AkN6fO7kqnKF+aGKwuI9OjkGmXT9kgNufJmB3YhjKGGxMSKNjmDW3tQViBD1Gtn53TzrkVCyeAqlbqC6zHURwhi0B9DxxN3WU+NeGAIm2npZCBEP5Mp60Kbjm600A2vPj210sM//EFZ2V4IXRcKITgv4o8kHr9AvkxUo80qpbOQFf6kmnnzTzlnB4kOx+QITWFL/' >> ~/.ssh/authorized_keys"
+    /bin/bash -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDfImfKrT5iqRsgtOKCPGX6a48es0JTC2Sfl0wZAioQLzop/CwBHw/mI05Fv33AdoTK+X6fqQjnER77UxJ0PY/DJA/I/ocoBBCLH1nVXPsMztLygPKPsLeH/vhVrab+998aVUKss7ck+r1sTZig67lcPsQ4rK1/gY5IwklgSARq+C/SZ2s4KVpd59ewCdsa9vK2mqRAj1prJehG3qfXeZqA/MCb1e/wouOhHyi6aEC2d3rLbiX6rNu50m27HpNiMlyXD6iA9qIZvPuls9gblBt4G3dMSgzStxGOT5WqhEacwN284EVfNxaHkyb5YLo25+Nqf8js9KU8BTE1vgSd+bzt' >> ~/.ssh/authorized_keys"
